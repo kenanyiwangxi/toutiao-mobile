02e481b2d995b78099bc9c6376ff622c8136f585
kenanyiwangxi <3215136710@qq.com>
2023-02-17 18:17:59 +0800

    实现了打包发布

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-  // devServer: {
-  //   proxy: {
-  //     '/ws': {
-  //       target: 'http://toutiao.itheima.net/',
-  //       pathRewrite: { '^/ws': '' },
-  //       ws: true,
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
+  transpileDependencies: true,
+  // 增强 vue-cli 的 webpack 配置项
+  configureWebpack: {
+    // 打包优化
+    externals: {
+      // import 时的包名称: window 全局的成员名称
+      'highlight.js': 'hljs',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      vant: 'vant',
+      dayjs: 'dayjs',
+      'socket.io-client': 'io',
+      popmotion: 'popmotion'
+    }
+  }
 })
